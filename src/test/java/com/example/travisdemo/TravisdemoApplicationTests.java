b59b1b961db8683266e2f09011a6ed4eaa5bf53b
Sunil Vishnu Bhandari <sunil.vb@Triconinfotech.com>
2021-06-18 15:04:31 +0530

    added test failure

--- src/test/java/com/example/travisdemo/TravisdemoApplicationTests.java
+++ src/test/java/com/example/travisdemo/TravisdemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.travisdemo;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class TravisdemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void failTest() {
+		fail();
 	}
 
 }
