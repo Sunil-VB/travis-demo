3fab3c05bfa64cfbb614c89a7b85f836c4858d3e
Sunil Vishnu Bhandari <sunil.vb@Triconinfotech.com>
2021-06-18 14:40:35 +0530

    initial commit

---
+++ src/main/java/com/example/travisdemo/TravisdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.travisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisdemoApplication.class, args);
+	}
+
+}
